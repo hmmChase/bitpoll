c483c6ac75d1e220434e6af00671b4e1959b7f27
Chase <hmmChase@users.noreply.github.com>
2018-05-24 21:25:50 -0600

    Add GitHub auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,78 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import firebase, { auth, provider } from '../../utils/firebase';
+import * as actions from '../../actions/index';
 import './App.css';
 
-class App extends Component {
+export class App extends Component {
+  componentDidMount() {
+    // auth.onAuthStateChanged(user => {
+    //   const cleanUser = {
+    //     userId: user.providerData[0].uid,
+    //     displayName: user.providerData[0].displayName
+    //     // displayName: 'Chase',
+    //     // userId: 10532835
+    //   };
+    //   this.props.logIn(cleanUser);
+    //   // if (user) {
+    //   //   this.setState({ user });
+    //   // }
+    // });
+  }
+
+  login = () => {
+    // auth.signInWithPopup(provider).then(result => {
+    //   const user = result.user;
+    //   console.log(user);
+    const cleanUser = {
+      // userId: user.providerData[0].uid,
+      // displayName: user.providerData[0].displayName
+      displayName: 'Chase',
+      userId: 10532835
+    };
+    this.props.logIn(cleanUser);
+    // });
+  };
+
+  logout = () => {
+    // auth.signOut().then(() => {
+    // this.setState({
+    //   // user: null
+    //   displayName: '',
+    //   userId: ''
+    // });
+    // });
+    this.props.logOut();
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          {this.props.userId ? (
+            <div>
+              <button onClick={this.logout}>Log Out</button>
+              <h3>Welcome, {this.props.displayName}</h3>
+            </div>
+          ) : (
+            <button onClick={this.login}>Log In</button>
+          )}
+          <h1 className="App-title">Bitpoll</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Poll</p>
       </div>
     );
   }
 }
 
-export default App;
+export const mapStateToProps = state => ({
+  displayName: state.user.displayName,
+  userId: state.user.userId
+});
+
+export const mapDispatchToProps = dispatch => ({
+  logIn: user => dispatch(actions.logIn(user)),
+  logOut: () => dispatch(actions.logOut())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/private/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyBtlh3EWl68895pmLKVoHpw95G2QYKsYf8',
+  authDomain: 'bit-poll.firebaseapp.com',
+  databaseURL: 'https://bit-poll.firebaseio.com',
+  projectId: 'bit-poll',
+  storageBucket: 'bit-poll.appspot.com',
+  messagingSenderId: '1009809453468'
+};
