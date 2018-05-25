f6871750f29bde8917e6bf6fd7529f186713db9f
Chase <hmmChase@users.noreply.github.com>
2018-05-24 21:25:25 -0600

    Add logIn and logOut actions

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,8 +2,12 @@ const initialState = {};
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_USER':
+    case 'LOG_IN':
       return { ...state, ...action.user };
+    case 'LOG_OUT':
+      console.log('logout3');
+
+      return { ...state, displayName: '', userId: '' };
 
     default:
       return state;
