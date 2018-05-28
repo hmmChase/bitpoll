3b346f875968d2cc6896b6200bcba781436b10e4
Chase <hmmChase@users.noreply.github.com>
2018-05-28 16:56:11 -0600

    Refactor moving header items into Header, and poll items into Poll

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,78 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import firebase, { auth, provider } from '../../utils/firebase';
+import firebase from '../../utils/firebase';
 import * as actions from '../../actions/index';
+import Header from '../Header/Header';
+import Poll from '../Poll/Poll';
+import { doFetch, parseLinkHeader } from '../../api';
 import './App.css';
 
 export class App extends Component {
-  componentDidMount() {
-    // auth.onAuthStateChanged(user => {
-    //   const cleanUser = {
-    //     userId: user.providerData[0].uid,
-    //     displayName: user.providerData[0].displayName
-    //     // displayName: 'Chase',
-    //     // userId: 10532835
-    //   };
-    //   this.props.logIn(cleanUser);
-    //   // if (user) {
-    //   //   this.setState({ user });
-    //   // }
-    // });
-  }
+  componentDidMount = () => {};
 
-  login = () => {
-    // auth.signInWithPopup(provider).then(result => {
-    //   const user = result.user;
-    //   console.log(user);
-    const cleanUser = {
-      // userId: user.providerData[0].uid,
-      // displayName: user.providerData[0].displayName
-      displayName: 'Chase',
-      userId: 10532835
-    };
-    this.props.logIn(cleanUser);
-    // });
-  };
+  // how to move to seperate file when need access to store
+  fetchContributors = async url => {
+    const response = await doFetch(url);
+    const contributors = await response.json();
 
-  logout = () => {
-    // auth.signOut().then(() => {
-    // this.setState({
-    //   // user: null
-    //   displayName: '',
-    //   userId: ''
-    // });
-    // });
-    this.props.logOut();
+    const cleanedContributors = contributors.map(contributor => {
+      return { [contributor.id]: { ...contributor } };
+    });
+
+    this.props.storeContributors(cleanedContributors);
+
+    const links = await response.headers.get('Link');
+    const parsedLinks = parseLinkHeader(links);
+    let next = parsedLinks.next;
+
+    if (next) {
+      await this.fetchContributors(next);
+    }
   };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {this.props.userId ? (
-            <div>
-              <button onClick={this.logout}>Log Out</button>
-              <h3>Welcome, {this.props.displayName}</h3>
-            </div>
-          ) : (
-            <button onClick={this.login}>Log In</button>
-          )}
-          <h1 className="App-title">Bitpoll</h1>
-        </header>
-        <p className="App-intro">Poll</p>
+        <Header fetchContributors={this.fetchContributors} />
+        <button
+          onClick={() =>
+            this.fetchContributors(
+              'https://api.github.com/repos/bitcoin/bitcoin/contributors?per_page=100'
+            )
+          }
+        >
+          Fetch contributors
+        </button>
+        <main>
+          <Poll />
+        </main>
       </div>
     );
   }
 }
 
 export const mapStateToProps = state => ({
-  displayName: state.user.displayName,
-  userId: state.user.userId
+  contributors: state.contributors
 });
 
 export const mapDispatchToProps = dispatch => ({
-  logIn: user => dispatch(actions.logIn(user)),
-  logOut: () => dispatch(actions.logOut())
+  storeContributors: contributors =>
+    dispatch(actions.storeContributors(contributors))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/Header/Header.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import firebase, { auth, provider } from '../../utils/firebase';
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import * as actions from '../../actions/index';
+import './Header.css';
+
+fontawesome.library.add(faGithub);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // auth.onAuthStateChanged(async user => {
+    //   if (user) {
+    //     if (this.props.userId === undefined) {
+    //       this.props.logIn(user);
+    //     }
+    //     console.log('cdm-before: ', this.props.contributors.length);
+    //     if (this.props.contributors.length === 0) {
+    //       this.fetchContributors(
+    //         'https://api.github.com/repos/bitcoin/bitcoin/contributors?page=1&per_page=100'
+    //       );
+    //       console.log('cdm-after: ', this.props.contributors.length);
+    //     }
+    //   }
+    // });
+  }
+
+  login = async () => {
+    const user = await this.authGitHub();
+    const cleanUser = {
+      userId: user.providerData[0].uid,
+      displayName: user.providerData[0].displayName
+    };
+    this.props.logIn(cleanUser);
+    this.props.fetchContributors(
+      'https://api.github.com/repos/bitcoin/bitcoin/contributors?page=1&per_page=100'
+    );
+  };
+
+  logout = () => {
+    auth.signOut().then(() => {
+      this.props.logOut();
+    });
+  };
+
+  authGitHub = () => {
+    return auth.signInWithPopup(provider).then(result => {
+      return result.user;
+    });
+  };
+
+  render() {
+    return (
+      <header>
+        <h1 className="App-title">Bitpoll</h1>
+        {this.props.userId ? (
+          <div className="signed-in-state">
+            <span className="welcome-text">
+              Welcome {this.props.displayName}
+            </span>
+
+            <div className="sign-out-btn" onClick={this.logout}>
+              <span>Log out</span>
+            </div>
+          </div>
+        ) : (
+          <div className="sign-in-btn" onClick={this.login}>
+            <FontAwesomeIcon icon={['fab', 'github']} />
+            <span className="sign-in-btn-txt">Sign in with GitHub</span>
+          </div>
+        )}
+      </header>
+    );
+  }
+}
+
+export const mapStateToProps = state => ({
+  displayName: state.user.displayName,
+  userId: state.user.userId,
+  contributors: state.contributors
+});
+
+export const mapDispatchToProps = dispatch => ({
+  logIn: user => dispatch(actions.logIn(user)),
+  logOut: () => dispatch(actions.logOut()),
+  storeContributors: contributors =>
+    dispatch(actions.storeContributors(contributors))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
