3122624a321d5ce5ae5a511592f7e2c4acd82421
Chase <hmmChase@users.noreply.github.com>
2018-05-28 20:58:35 -0600

    Sync vote count to firebase

--- src/components/Poll/Poll.js
+++ src/components/Poll/Poll.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import firebase from '../../utils/firebase';
+
 import './Poll.css';
 
 class Poll extends Component {
@@ -6,16 +8,48 @@ class Poll extends Component {
     super(props);
 
     this.state = {
-      answer: null,
-      disabled: true
+      option1: 0,
+      option2: 0,
+      selected: null,
+      value: null,
+      disabled: true,
+      voted: false
     };
   }
 
-  componentDidMount = () => {};
+  componentDidMount = () => {
+    // get refs
+    const pollRef = firebase
+      .database()
+      .ref()
+      .child('poll');
+    const option1Ref = pollRef.child('option1');
+    const option2Ref = pollRef.child('option2');
+
+    // sync changes
+    option1Ref.on('value', snapshot => {
+      let option1RefVal = snapshot.val();
+
+      this.setState({
+        option1: option1RefVal
+      });
+    });
+
+    option2Ref.on('value', snapshot => {
+      let option2RefVal = snapshot.val();
+
+      this.setState({
+        option2: option2RefVal
+      });
+    });
+  };
 
   handleChange = event => {
+    const { name, value } = event.target;
+
     this.setState({
-      answer: event.target.value,
+      selected: name,
+      value,
       disabled: false
     });
   };
@@ -23,40 +57,66 @@ class Poll extends Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    alert(this.state.answer);
+    this.increment(event.target.name);
   };
+
+  increment = name => {
+    this.setState({
+      [this.state.selected]: this.state[this.state.selected]++
+    });
+
+    const pollRef = firebase
+      .database()
+      .ref()
+      .child('poll');
+
+    pollRef.update({
+      [this.state.selected]: this.state[this.state.selected]
+    });
+
+    // const option1Ref = pollRef.child('option1');
+    // const option2Ref = pollRef.child('option2');
+    // if (this.state.selected === 'option1') {
+    //   option1Ref.set(this.state[this.state.selected]++);
+    // }
+    // if (this.state.selected === 'option2') {
+    //   option2Ref.set(this.state[this.state.selected]++);
+    // }
+    // console.log(this.state[this.state.selected]++);
+  };
+
+  displayResults = () => {};
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <p>Should the blocksize be increased?</p>
-
+        <h3>Should the blocksize be increased?</h3>
         <ul>
           <li>
             <input
               className="option"
               id="option1"
+              name="option1"
               type="radio"
               value="yes"
-              checked={this.state.answer === 'yes'}
+              checked={this.state.value === 'yes'}
               onChange={this.handleChange}
             />
             <label htmlFor="option1">Yes</label>
           </li>
-
           <li>
             <input
               className="option"
               id="option2"
+              name="option2"
               type="radio"
               value="no"
-              checked={this.state.answer === 'no'}
+              checked={this.state.value === 'no'}
               onChange={this.handleChange}
             />
             <label htmlFor="option2">No</label>
           </li>
         </ul>
-
         <button
           className="submit-btn"
           type="submit"
