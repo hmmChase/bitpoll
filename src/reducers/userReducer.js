a4ab6f514b2e0ddf2023e5d0cb1ee3b3958b2423
Chase <hmmChase@users.noreply.github.com>
2018-06-03 22:55:54 -0600

    Add vote checking functionality

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,14 +1,16 @@
-const initialState = { isContributor: false };
+const initialState = { isContributor: false, ifVoted: false };
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-  case 'STORE_USER':
-    return { ...state, ...action.user };
-  case 'STORE_LOG_OUT':
-    return { ...state, displayName: '', userId: '' };
-  case 'STORE_IS_CONTRIBUTOR':
-    return { ...state, isContributor: action.boolean };
-  default:
-    return state;
+    case 'STORE_USER':
+      return { ...state, ...action.user };
+    case 'STORE_LOG_OUT':
+      return { ...state, displayName: '', userId: '' };
+    case 'STORE_IS_CONTRIBUTOR':
+      return { ...state, isContributor: action.boolean };
+    case 'STORE_IF_VOTED':
+      return { ...state, ifVoted: action.boolean };
+    default:
+      return state;
   }
 };
