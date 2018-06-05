c9132d3f1f9a00f7d6265e51addddb822259deb2
Chase <hmmChase@users.noreply.github.com>
2018-06-05 08:17:34 -0600

    Remove uneeded fetchContributors function

--- src/api/index.js
+++ src/api/index.js
@@ -10,14 +10,19 @@ export const doFetch = async url => {
   }
 };
 
+export const cleanContributors = contributors => {
+  return contributors.reduce((contributersObj, contributor) => {
+    contributersObj[contributor.id] = contributor;
+    return contributersObj;
+  }, {});
+};
+
 export const parseLinkHeader = header => {
   if (header.length === 0) {
     throw new Error('input must not be of zero length');
   }
-  // Split parts by comma
   const parts = header.split(',');
   const links = {};
-  // Parse each part into a named link
   for (let i = 0; i < parts.length; i++) {
     const section = parts[i].split(';');
     if (section.length !== 2) {
@@ -28,27 +33,4 @@ export const parseLinkHeader = header => {
     links[name] = url;
   }
   return links;
-};
-
-export const cleanContributors = contributors => {
-  return contributors.reduce((contributersObj, contributor) => {
-    contributersObj[contributor.id] = contributor;
-    return contributersObj;
-  }, {});
-};
-
-export const fetchContributors = async url => {
-  const response = await doFetch(url);
-  const contributors = await response.json();
-  const cleanedContributors = cleanContributors(contributors);
-
-  this.props.storeContributors(cleanedContributors);
-
-  const links = await response.headers.get('Link');
-  const parsedLinks = parseLinkHeader(links);
-  let next = parsedLinks.next;
-
-  if (next) {
-    await this.fetchContributors(next);
-  }
 };
