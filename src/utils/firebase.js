a71888eccf339022334ca8c61a6b32a74bef953c
Chase <hmm@email.com>
2018-08-28 15:20:28 -0600

    Update firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,13 @@
 import firebase from 'firebase';
-import { config } from '../private/firebase';
+
+const config = {
+  apiKey: 'AIzaSyBtlh3EWl68895pmLKVoHpw95G2QYKsYf8',
+  authDomain: 'bit-poll.firebaseapp.com',
+  databaseURL: 'https://bit-poll.firebaseio.com',
+  projectId: 'bit-poll',
+  storageBucket: 'bit-poll.appspot.com',
+  messagingSenderId: '1009809453468'
+};
 
 firebase.initializeApp(config);
 
